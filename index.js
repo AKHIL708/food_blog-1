f28e35e91b17cc133127f17019dac1ede25bdda6
annonymous <aakhiljay06@gmail.come>
2022-04-30 16:59:50 +0530

    add to git respository

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("happy coding");
+let count = 0;
+function add(){
+    count += 1;
+      document.getElementById("demo").innerHTML = count;
+}
+
+function minus(){
+     count -= 1;
+     document.getElementById("demo").innerHTML = count;
+}
+
+function add(){
+    count += 1;
+      document.getElementById("demo2").innerHTML = count;
+}
+
+function minus(){
+     count -= 1;
+     document.getElementById("demo2").innerHTML = count;
+
+}
+
+
